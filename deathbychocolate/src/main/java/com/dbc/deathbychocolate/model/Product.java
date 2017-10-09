4b52b10c57fe6f9f40fa882c611e9be8f54e3a4e
Amritavijayakumar <vibithra95@gmail.com>
2017-10-09 17:26:59 +0530

    initial project

---
+++ deathbychocolate/src/main/java/com/dbc/deathbychocolate/model/Product.java
@@ -0,0 +1,50 @@
+package com.dbc.deathbychocolate.model;
+
+public class Product {
+	private int productId,productQuality;
+	private String productName,productCategory,productSupplier,productDescription;
+	private double productPrice;
+	public int getProductId() {
+		return productId;
+	}
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+	public int getProductQuality() {
+		return productQuality;
+	}
+	public void setProductQuality(int productQuality) {
+		this.productQuality = productQuality;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public String getProductCategory() {
+		return productCategory;
+	}
+	public void setProductCategory(String productCategory) {
+		this.productCategory = productCategory;
+	}
+	public String getProductSupplier() {
+		return productSupplier;
+	}
+	public void setProductSupplier(String productSupplier) {
+		this.productSupplier = productSupplier;
+	}
+	public String getProductDescription() {
+		return productDescription;
+	}
+	public void setProductDescription(String productDescription) {
+		this.productDescription = productDescription;
+	}
+	public double getProductPrice() {
+		return productPrice;
+	}
+	public void setProductPrice(double productPrice) {
+		this.productPrice = productPrice;
+	}
+
+}
---
+++ deathbychocolate/src/main/java/com/dbc/deathbychocolate/model/UserRegisteration.java
@@ -0,0 +1,50 @@
+package com.dbc.deathbychocolate.model;
+
+public class UserRegisteration {
+	private String userName,userEmail,userCity,userAddress;
+	private long userContact;
+	private int userAge,userPincode;
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserEmail() {
+		return userEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+	public String getUserCity() {
+		return userCity;
+	}
+	public void setUserCity(String userCity) {
+		this.userCity = userCity;
+	}
+	public String getUserAddress() {
+		return userAddress;
+	}
+	public void setUserAddress(String userAddress) {
+		this.userAddress = userAddress;
+	}
+	public long getUserContact() {
+		return userContact;
+	}
+	public void setUserContact(long userContact) {
+		this.userContact = userContact;
+	}
+	public int getUserAge() {
+		return userAge;
+	}
+	public void setUserAge(int userAge) {
+		this.userAge = userAge;
+	}
+	public int getUserPincode() {
+		return userPincode;
+	}
+	public void setUserPincode(int userPincode) {
+		this.userPincode = userPincode;
+	}
+
+}
